a170f73165d7236fa9af25e2a4a122ac603aaf78
Rain.L <rainlei@sjfood.us>
2025-03-04 23:56:27 -0800

    AsyncUDP

--- software/hexapod_arduino/hexapod_arduino.ino
+++ software/hexapod_arduino/hexapod_arduino.ino
@@ -2,6 +2,9 @@
 #include "MotionController.h"
 #include "config.h"
 
+#include "WiFi.h"
+#include "AsyncUDP.h"
+
 #define PS2_DAT 19
 #define PS2_CMD 23
 #define PS2_SEL 5
@@ -9,21 +12,30 @@
 
 MotionController motionController;
 PS2Handler ps2;
-
+AsyncUDP udpServer;
 PS2X ps2x;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   motionController.begin(60);
-  ps2.begin(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT);
+  // ps2.begin(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("Home2.4G", "a278742221");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected WIFI");
+  if (udpServer.listen(1234)) {
+    udpServer.onPacket([](AsyncUDPPacket packet) {
+      String data = packet.readString();
+      Serial.println(data);
+      int pwm = 0, channel = 0, value = 0, offset = 0;
+      int numFields = sscanf(data.c_str(), "%d,%d,%d,%d", &pwm, &channel, &value, &offset);
+      motionController.single_debugging(pwm, channel, value, offset);
+    });
+  }
 }
-
-void loop()
-{
-  MotionMode cmd = ps2.convertMotionCommand();
-
-  motionController.exec_next_motion(cmd);
-
-  delay(10); // PS2 add new
+void loop() {
+  delay(1000);
 }
